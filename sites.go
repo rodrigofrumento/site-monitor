71cc28f035ad00b6a474268f6618de9c4c730528
rodrigofrumento <rodrigo.frumento@gmail.com>
2022-12-13 22:23:24 -0300

    Finish Project

--- sites.go
+++ sites.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -11,19 +16,18 @@ const numMonit = 3
 const seg = 5
 
 func main() {
-
 	showIntro()
+
 	for {
 
 		showMenu()
-
 		command := readCommand()
-
 		switch command {
 		case 1:
 			startMonitoring()
 		case 2:
 			fmt.Println("Showing Logs...")
+			showLogs()
 		case 0:
 			fmt.Println("Exiting...")
 			os.Exit(0)
@@ -57,10 +61,7 @@ func readCommand() int {
 
 func startMonitoring() {
 	fmt.Println("Monitoring...")
-	sites := []string{
-		"site1",
-		"site2",
-		"site3"}
+	sites := readSitesFromDoc()
 
 	for i := 0; i < numMonit; i++ {
 		fmt.Println("Starting next monitoring")
@@ -83,4 +84,45 @@ func siteTesting(site string) {
 	} else {
 		fmt.Println("Site:", site, "something goes wrong:", res.StatusCode)
 	}
+}
+
+func readSitesFromDoc() []string {
+	var sites []string
+	websites, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro", err)
+	}
+
+	reader := bufio.NewReader(websites)
+
+	for {
+		row, err := reader.ReadString('\n')
+		row = strings.TrimSpace(row)
+		sites = append(sites, row)
+
+		if err == io.EOF {
+			break
+		}
+	}
+	websites.Close()
+	return sites
+}
+
+func logWriting(site string, status bool) {
+	file, err := os.OpenFile("log.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Ocorreu erro", err)
+	}
+	file.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+	file.Close()
+}
+
+func showLogs() {
+	file, err := ioutil.ReadFile("log.txt")
+	if err != nil {
+		fmt.Println("Ocorreu erro", err)
+	}
+	fmt.Println(string(file))
 }
